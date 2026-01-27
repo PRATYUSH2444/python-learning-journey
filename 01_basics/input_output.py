1569e96458ea4f857b0b7f779d3995c7d4741b15
Pratyush Prakash <prakash.pratyush20@gmail.com>
2026-01-27 20:22:26 +0530

    input/output examples

--- 01_basics/input_output.py
+++ 01_basics/input_output.py
@@ -1,11 +1,16 @@
-'''
-'''
+#pyhton dont have multiline comment but we can use triple quotes for that , *whihc means it is becoming string literal not comment*
+
 
 #input and output : input() and print() functions
-name = input("Enter your name: ")  # input() function to take input from user
-collegeName = input("Enter your college name: ")
-print("Hello, " + name + "!")  # print() function to display output
-print("Welcome to " + collegeName + ".")
+#name = input("Enter your name: ")  # input() function to take input from user
+#collegeName = input("Enter your college name: ")
+#print("Hello, " + name + "!")  # print() function to display output
+#print("Welcome to " + collegeName + ".")
+#print(f"Hello, {name}! Welcome to {collegeName}.")  # Using f-string for formatted output
+
+# raw string
+#raw strings : r or R before quotes
 
 
-
+age = int(input("Hello what is your age? \n"))
+print(f"the entered age is {age}")
